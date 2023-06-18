336ca9f1a0d83380c890bcb07337e61b330d7548
Никита Федяев <itsfedos@gmail.com>
2023-06-18 22:38:24 +0300

    schemas charity_project.py

--- app/schemas/charity_project.py
+++ app/schemas/charity_project.py
@@ -1,17 +1,37 @@
-from pydantic import BaseModel, Field
+from datetime import datetime
+from typing import Optional
+
+from pydantic import BaseModel, Field, Extra, PositiveInt
 
 
 class CharityProjectBase(BaseModel):
-    ...
+    name: Optional[str] = Field(None, min_length=1, max_length=100)
+    description: Optional[str] = Field(None, min_length=1)
+    full_amount: Optional[PositiveInt]
+
+    class Config:
+        # запрет принимать параметры, которые не описаны в схеме
+        extra = Extra.forbid
 
 
 class CharityProjectCreate(CharityProjectBase):
-    ...
+    name: str = Field(..., min_length=1, max_length=100)
+    description: str = Field(..., min_length=1)
+    full_amount: PositiveInt
 
 
 class CharityProjectUpdate(CharityProjectBase):
-    ...
+    pass
 
 
-class CharityProjectDB(CharityProjectBase):
-    ...
+class CharityProjectDB(CharityProjectCreate):
+    id: int
+    invested_amount: int
+    fully_invested: bool
+    create_date: datetime
+    close_date: Optional[datetime]
+
+    class Config:
+        # для того, чтобы можно было сериализовать объекты ORM,
+        # а не только Python-словарь или JSON
+        orm_mode = True
