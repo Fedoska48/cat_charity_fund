1705add4c2df01ff632575b57f03e7dccd9c0a21
Никита Федяев <itsfedos@gmail.com>
2023-06-18 23:28:33 +0300

    schemas donation.py

--- app/schemas/donation.py
+++ app/schemas/donation.py
@@ -1,17 +1,34 @@
-from pydantic import BaseModel, Field
+from datetime import datetime
+from typing import Optional
+
+from pydantic import BaseModel, PositiveInt, Extra
 
 
 class DonationBase(BaseModel):
-    ...
+    full_amount: PositiveInt
+    comment: Optional[str]
+
+    class Config:
+        # запрет принимать параметры, которые не описаны в схеме
+        extra = Extra.forbid
 
 
 class DonationCreate(DonationBase):
-    ...
+    pass
 
 
-class DonationUpdate(DonationBase):
-    ...
+class DonationShortDB(DonationBase):
+    id: int
+    create_date: datetime
+
+    class Config:
+        # для того, чтобы можно было сериализовать объекты ORM,
+        # а не только Python-словарь или JSON
+        orm_mode = True
 
 
-class DonationDB(DonationBase):
-    ...
+class DonationFullDB(DonationShortDB):
+    user_id: str
+    invested_amount: int
+    fully_invested: bool
+    close_date: Optional[datetime]
