2d4acd033b3117d518bb13f25512d8b7a5883748
Никита Федяев <itsfedos@gmail.com>
2023-06-20 22:29:49 +0300

    get_multi CRUDBase

--- app/crud/base.py
+++ app/crud/base.py
@@ -1,1 +1,21 @@
 """Базовые операциии CRUD."""
+
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+
+class CRUDBase:
+    """Для соблюдения стиля DRY, создаем базовый класс для CRUD-операций."""
+
+    def __init__(self, model):
+        """При инициализации класса присваивается модель
+        и далее все операции производятся над моделью."""
+        self.model = model
+
+    async def get_multi(
+            self,
+            session: AsyncSession
+    ):
+        """Абстрактный метод для получения списка объектов."""
+        db_objs = await session.execute(select(self.model))
+        return db_objs.scalars().all()
