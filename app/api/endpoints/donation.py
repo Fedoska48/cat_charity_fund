77a2ecc1c713f219c093520255013d69d0c70099
Никита Федяев <itsfedos@gmail.com>
2023-07-02 16:46:33 +0300

    donation endpoints, method get_by_user

--- app/api/endpoints/donation.py
+++ app/api/endpoints/donation.py
@@ -4,8 +4,11 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.core.db import get_async_session
+from app.core.user import current_user
 from app.crud.donation import donation_crud
-from app.schemas.donation import DonationFullDB
+from app.models import User
+from app.schemas.donation import DonationFullDB, DonationShortDB, \
+    DonationCreate
 
 router = APIRouter()
 
@@ -23,6 +26,29 @@ async def get_all_charity_projects(
     all_donations = await donation_crud.get_multi(session)
     return all_donations
 
-# @TODO create_donation
 
-# @TODO get_user_donations
+@router.post(
+    '/',
+    response_model=DonationShortDB,
+    response_model_exclude_none=True
+)
+async def create_donation(
+        obj_in: DonationCreate,
+        session: AsyncSession = Depends(get_async_session)
+):
+    #  @TODO: some validations before create
+    created_donation = await donation_crud.create(obj_in, session)
+    return created_donation
+
+
+@router.get(
+    '/my',
+    response_model=List[DonationShortDB],
+    response_model_exclude={'user_id'}
+)
+async def get_user_donations(
+        user: User = Depends(current_user),
+        session: AsyncSession = Depends(get_async_session)
+):
+    my_donations = await donation_crud.get_by_user(user, session)
+    return my_donations
--- app/crud/donation.py
+++ app/crud/donation.py
@@ -1,9 +1,24 @@
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
 from app.crud.base import CRUDBase
+from app.models import User
 from app.models.donation import Donation
 
 
 class CRUDDonation(CRUDBase):
-    pass
+
+    @staticmethod
+    async def get_by_user(
+            user: User,
+            session: AsyncSession
+    ):
+        """Список донатов по пользователю."""
+        donations = await session.execute(
+            select(Donation).where(Donation.user_id == user.id)
+        )
+        donations = donations.scalars().all()
+        return donations
 
 
 donation_crud = CRUDDonation(Donation)
