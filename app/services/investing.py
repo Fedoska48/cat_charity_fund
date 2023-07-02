b1be4bb49895d4086a7e33597d56f6188cc6ba1a
Никита Федяев <itsfedos@gmail.com>
2023-07-02 21:39:26 +0300

    investing additional methods

--- app/services/investing.py
+++ app/services/investing.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+from select import select
 from typing import Union, List
 
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -22,11 +24,21 @@ async def get_not_invested_objects(
         session: AsyncSession
 ) -> List[Union[CharityProject, Donation]]:
     """Получить объекты, которые еще не полностью проинвестированы."""
-    ...
+    not_invested_objects = await session.execute(
+        select(
+            obj_in
+        ).where(
+            obj_in.fully_invested == False  # noqa
+        ).order_by(
+            obj_in.create_date
+        )
+    )
+    return not_invested_objects.scalars().all()
 
 
 async def close_fully_invested_object(
         obj_in: Union[CharityProject, Donation],
 ) -> None:
     """Закрывает объект инвестирования."""
-    ...
+    obj_in.fully_invested = True
+    obj_in.close_date = datetime.now()
