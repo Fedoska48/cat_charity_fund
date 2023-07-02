eabaf8d03f922a9bba3a60bc4cc0596514d75988
Никита Федяев <itsfedos@gmail.com>
2023-07-02 22:35:40 +0300

    investing update

--- app/services/investing.py
+++ app/services/investing.py
@@ -16,7 +16,35 @@ async def investing(
     Увеличение invested_amount, как в проектах, так и в пожертвованиях.
     Установка значений fully_invested и close_date, при необходимости.
     """
-    ...
+    model_for_invest = (
+        CharityProject if isinstance(obj_in, Donation) else Donation
+    )
+    not_invested_objects = await get_not_invested_objects(
+        model_for_invest,
+        session
+    )
+    if not_invested_objects:
+        available = obj_in.fully_invested
+        for open_object in not_invested_objects:
+            needs_amount = (open_object.full_amount -
+                            open_object.invested_amount)
+            ready_for_invest = (
+                obj_in.full_amount
+                if needs_amount > obj_in.full_amount
+                else needs_amount
+            )
+            available -= ready_for_invest
+            open_object.invested_amount += ready_for_invest
+            obj_in.invested_amount += ready_for_invest
+
+            if open_object.invested_amount == open_object.full_amount:
+                await close_fully_invested_object(open_object)
+
+            if not available:
+                await close_fully_invested_object(obj_in)
+                break
+        await session.commit()
+    return obj_in
 
 
 async def get_not_invested_objects(
