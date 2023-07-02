7910072c835d4bd415c65eafe1e75be7f66f5c35
Никита Федяев <itsfedos@gmail.com>
2023-07-02 17:03:09 +0300

    user endpoints update

--- app/api/routers.py
+++ app/api/routers.py
@@ -2,7 +2,7 @@
 from fastapi import APIRouter
 
 from app.api.endpoints import charity_project_router, \
-    donation_router  # user_router
+    donation_router, user_router
 
 main_router = APIRouter()
 main_router.include_router(
@@ -15,4 +15,4 @@ main_router.include_router(
     prefix='/donation',
     tags=['Donations']
 )
-# main_router.include_router(user_router)
+main_router.include_router(user_router)
